5e0b9591141012d53ca036241fde956d575442fc
Eduardo P de Sousa <edupsousa@gmail.com>
2021-08-03 11:19:42 -0300

    botão cancelar oculta a caixa novo evento

--- agenda.js
+++ agenda.js
@@ -3,3 +3,8 @@ function novoEvento() {
     var novoEvento = document.getElementById('novoEvento');
     novoEvento.classList.remove('d-none');
 }
+
+function fecharNovoEvento() {
+    var novoEvento = document.getElementById('novoEvento');
+    novoEvento.classList.add('d-none');
+}
