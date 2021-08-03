b85f5d17b5b2ec4b0ed7b53358e99db4e072c503
Eduardo P de Sousa <edupsousa@gmail.com>
2021-08-03 14:34:48 -0300

    gravação aula 2

--- agenda.js
+++ agenda.js
@@ -1,5 +1,36 @@
+var buttonNovoEvento = document.getElementById('buttonNovoEvento');
+var buttonCancelar = document.getElementById('buttonCancelar');
+var novoEvento = document.getElementById('novoEvento');
+var formNovoEvento = document.getElementById('formNovoEvento');
+var inputNomeEvento = document.getElementById('nomeEvento');
+var inputDataEvento = document.getElementById('dataEvento');
 
-function novoEvento() {
-    var novoEvento = document.getElementById('novoEvento');
+function mostrarNovoEvento() {
     novoEvento.classList.remove('d-none');
 }
+
+function ocultarNovoEvento() {
+    novoEvento.classList.add('d-none');
+}
+
+function novoEventoValido(nomeEvento, dataEvento) {
+    // nome evento está vazio?
+    // data evento está vazia?
+    // data evento é no passado?
+    return true;
+}
+
+function salvarNovoEvento(event) {
+    event.preventDefault();
+    var nomeEvento = inputNomeEvento.value;
+    var dataEvento = new Date(inputDataEvento.value);
+    if (novoEventoValido(nomeEvento, dataEvento)) {
+        console.log('Evento é válido!');
+    } else {
+        console.log('Evento é inválido!');
+    }
+}
+
+buttonNovoEvento.addEventListener('click', mostrarNovoEvento);
+buttonCancelar.addEventListener('click', ocultarNovoEvento);
+formNovoEvento.addEventListener('submit', salvarNovoEvento);
